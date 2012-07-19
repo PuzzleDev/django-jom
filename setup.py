9e7d5bb2ff0308f5a71598e1a1fd70fae140ab35
Michele Sama <michele.sama@gmail.com>
2012-07-19 12:40:05 +0100

    Fixed JomInstance representation

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(name = 'django-puzzledev-jom',
        package_data = {
                 'jom': ['templates/*.*',
                         'templates/jom/*.*',
-                        'templates/jom/templatetags/*.*'],
+                        'templates/jom/templatetags/*.*',
+                        'static/*.*',
+                        'static/js/*.*'],
                 }
      )
